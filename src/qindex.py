ca89415cd11973d53dc532518fc2f1f19a6a72d6
meyassu <matthiaseyassu@gmail.com>
2023-11-05 13:11:31 -0500

    added rtree serialization/deserialization

--- src/qindex.py
+++ src/qindex.py
@@ -1,59 +1,58 @@
+import rtree
 from rtree import index
+import geopandas as gpd
 
 """
 Spatial indexing computations
 """
-def build_rtree(mbrs_gdf):
-	"""
-	Spatially indexes MBRs by building an R*-tree.
 
-	:param mbrs_gdf: (gpd.GeoDataFrame) -> contains all MBRs in geographical scope
+def _set_properties(fname='../data/rtree', overwrite=True, dat_extension='data', idx_extension='index'):
 
-	:return: (rtree.Index) -> the R*-tree
-	"""
+    properties = rtree.index.Property()
 
-	print('Building R*-tree...')
+    properties.filename = fname
+
+    properties.overwrite = overwrite
+
+    properties.dat_extension = dat_extension
+    properties.idx_extension = idx_extension
+
+    return properties
+
+
+def build_rtree(mbrs_gdf, properties):
+    """
+    Spatially indexes MBRs by building an R*-tree.
+
+    :param mbrs_gdf: (gpd.GeoDataFrame) -> contains all MBRs in geographical scope
+
+    :return: (rtree.Index) -> the R*-tree
+    """
+
+    print('Building R*-tree...')
+
 	# Populate R*-tree with MBRs
-	rtree = index.Index()
-	for i, row in mbrs_gdf.iterrows():
-		rtree.insert(i, row['geometry'].bounds)
+    rtree = index.Index(properties.filename, properties=properties)
+    for i, row in mbrs_gdf.iterrows():
+        rtree.insert(i, row['geometry'].bounds)
 
-	return rtree
+    return rtree
 
-def save_rtree(rtree, output_fpath):
-    """
-    Seralizes the rtree to a file.
-
-    :param rtree: (rtree.index) -> the rtree
-    :param output_fpath: (str) -> the output filepath
-
-    :return: None
-    """
-
-    # Serialize the R-tree to a file
-    with open(output_fpath, 'wb') as f:
-        rtree.serialize(f)
-
-def load_rtree(fpath):
+def load_rtree(properties):
     """
     Loads the rtree from file.
 	
 	:param fpath: (str) -> the rtree filepath
      
-     :return (rtree.Index) -> the rtree
+    :return (rtree.Index) -> the rtree
     """
 
-    # Load the serialized R-tree from a file
-    idx = index.Index(fpath)
-    return idx
-    # with open(fpath, 'rb') as f:
-    #     rtree = index.Index()
-    #     rtree.deserialize(f)
-
-    # return rtree
+    dat_file = properties.fname + properties.dat_extension
+    idx_file = properties.fname + properties.idx_extension
+    rtree = rtree.index.Rtree(dat_file, idx_file, properties=properties)
+    
+    return rtree
 
 
-
-
-
-#
+mbrs_gdf = gpd.read_file('../data/mbrs.geojson')
+rtree = build_rtree(mbrs_gdf, _set_properties())
