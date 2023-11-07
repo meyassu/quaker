d382de66f4dbad3a2ceeea7523ed0ec96d6f884c
meyassu <matthiaseyassu@gmail.com>
2023-11-07 14:10:23 -0500

    removed _set_properties() bc deserialization is buggy

--- src/qindex.py
+++ src/qindex.py
@@ -5,33 +5,7 @@ import geopandas as gpd
 """
 Spatial indexing computations
 """
-def _set_rtree_properties(fpath='../data/rtree', overwrite=True, dat_extension=None, idx_extension=None):
-    """
-    Sets rtree properties mostly relevant to serialization / deserialization.
-
-    :param fpath: (str) -> the rtree filepath
-    :param overwrite: (bool) -> whether to overwrite existing rtree files during serialization
-    :param dat_extension: (str) -> file extension for data file
-    :param idx_extension: (str) -> file extension for index file
-
-    :return: (rtree.index.Property) -> object storing all property preferences
-    """
-
-    properties = rtree.index.Property()
-
-    properties.filename = fpath
-
-    properties.overwrite = overwrite
-
-    if dat_extension != None:
-        properties.dat_extension = dat_extension
-    if idx_extension != None:
-        properties.idx_extension = idx_extension
-
-    return properties
-
-
-def build_rtree(mbrs_gdf, properties):
+def build_rtree(mbrs_gdf):
     """
     Spatially indexes MBRs by building an R*-tree.
 
@@ -43,7 +17,7 @@ def build_rtree(mbrs_gdf, properties):
     print('Building R*-tree...')
 
 	# Populate R*-tree with MBRs
-    rtree_obj = index.Index(properties.filename, properties=properties)
+    rtree_obj = index.Index()
     for i, row in mbrs_gdf.iterrows():
         rtree_obj.insert(i, row['geometry'].bounds)
 
