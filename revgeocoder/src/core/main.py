60b6ab23fe1b3a75767fa66dce436f2b8b2bef2d
meyassu <matthiaseyassu@gmail.com>
2023-11-16 16:46:31 -0500

    added DATA_DIR constant

--- revgeocoder/src/core/main.py
+++ revgeocoder/src/core/main.py
@@ -6,17 +6,21 @@ from ..utils.database import get_engine_rds
 from qindex import build_rtree
 from rgc import reverse_geocode
 
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '../../data/config/.env')
-load_dotenv(dotenv_path)
-
 """
 Constants
 """
-
+DATA_DIR = '../../data/'
 BATCH_SIZE = 1000
 
+
+dotenv_path = os.path.join(os.path.dirname(__file__), os.path.join(DATA_DIR, 'config/.env'))
+load_dotenv(dotenv_path)
+
 if __name__ == "__main__":
+    
+    # Validate user data
+
+
     # Get RDS engine
     rds_engine = get_engine_rds()
 
@@ -28,7 +32,6 @@ if __name__ == "__main__":
 
     # Create R*-tree
     rtree_obj = build_rtree(mbrs_gdf)
-
 
     # Run reverse geocoding algorithm
     reverse_geocode(rtree_obj, boundaries_gdf, table_name=os.get_env('DATA_TABLE_NAME'), batch_size=BATCH_SIZE, location_table_name=os.get_env('LOCATION_TABLE_NAME'), engine=rds_engine)
