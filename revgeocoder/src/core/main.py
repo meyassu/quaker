808ea3f2ce906371e02c16c4cc526d327585ba3c
meyassu <matthiaseyassu@gmail.com>
2023-11-16 21:05:21 -0500

    adjusted code more to work with container filesystem

--- revgeocoder/src/core/main.py
+++ revgeocoder/src/core/main.py
@@ -1,8 +1,10 @@
+import pandas as pd
 import geopandas as gpd
 from dotenv import load_dotenv
 import os
 
-from ..utils.database import get_engine_rds
+from ..utils.database import get_engine_rds, init_database_rds
+from ..utils.validate import validate_data
 from qindex import build_rtree
 from rgc import reverse_geocode
 
@@ -10,6 +12,8 @@ from rgc import reverse_geocode
 Constants
 """
 DATA_DIR = '../../data/'
+DATA_TABLE_NAME = os.get_env('DATA_TABLE_NAME')
+LOCATION_TABLE_NAME = os.get_env('LOCATION_TABLE_NAME')
 BATCH_SIZE = 1000
 
 
@@ -18,11 +22,19 @@ load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
     
+    # Load user data
+    data = pd.read_csv(os.path.join(DATA_DIR), 'data.csv')
+    
     # Validate user data
-
+    is_valid_data, message = validate_data(data)
+    if not is_valid_data:
+        raise Exception(f'{message}: {data}')
 
     # Get RDS engine
     rds_engine = get_engine_rds()
+
+    # Initialize database
+    init_database_rds(data, data_table_name=DATA_TABLE_NAME, location_table_name=LOCATION_TABLE_NAME)
 
     # Load boundaries data
     boundaries_gdf = gpd.read_file('../data/internal/boundaries.geojson')
@@ -34,7 +46,7 @@ if __name__ == "__main__":
     rtree_obj = build_rtree(mbrs_gdf)
 
     # Run reverse geocoding algorithm
-    reverse_geocode(rtree_obj, boundaries_gdf, table_name=os.get_env('DATA_TABLE_NAME'), batch_size=BATCH_SIZE, location_table_name=os.get_env('LOCATION_TABLE_NAME'), engine=rds_engine)
+    reverse_geocode(rtree_obj, boundaries_gdf, table_name=DATA_TABLE_NAME, batch_size=BATCH_SIZE, location_table_name=LOCATION_TABLE_NAME, engine=rds_engine)
 
 
 
