7cd411802a2823e1994b702d3335ab29c34d4600
meyassu <matthiaseyassu@gmail.com>
2023-11-25 16:27:13 -0500

    minor changes:

--- run-revgeocoder.sh
+++ run-revgeocoder.sh
@@ -10,9 +10,8 @@ fi
 # Get data directory path
 local_data_directory="$1"
 
-# Pull the Docker image
+# Pull latest Docker image
 sudo docker pull meyassu/revgeocoder:latest
 
 # Run the container with the data directory mounted
 sudo docker run --rm -v "$local_data_directory:/usr/src/app/user_data" meyassu/revgeocoder:latest
-
