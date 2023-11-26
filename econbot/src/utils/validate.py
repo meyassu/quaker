264c2ff99f3930ab728e9a25c24471dcb944534b
meyassu <matthiaseyassu@gmail.com>
2023-11-26 12:57:36 -0500

    added basic validation

---
+++ econbot/src/utils/validate.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from src import LOGGER
+
+def validate_data(data_fpath):
+    
+    LOGGER.info('Validating data...')
+    
+    is_valid = True
+    error_message = ""
+
+    data = pd.read_csv(data_fpath)
+
+    # Check for missing values
+    if data.isnull().values.any():
+        is_valid = False
+        error_message += 'Validation failed: Missing values detected' + '\n' 
+
+    # Check for duplicates
+    if data.duplicated().any():
+        is_valid = False
+        error_message += 'Validation failed: Duplicate records found' + '\n' 
+
+    # Check for empty records
+    if (data.applymap(lambda x: x == '')).any(axis=None):
+        is_valid = False
+        error_message += 'Validation failed: Empty records found' + '\n'
+
+    # Check for valid range of values
+    # Assuming the year should be between 1900 and 2100 and rGDP should be positive
+    if not data['year'].between(1900, 2100).all():
+        is_valid = False
+        error_message += 'Validation failed: Year out of valid range' + '\n'
+    
+    if (data['rGDP'] < 0).any():
+        is_valid = False
+        error_message += 'Validation failed: rGDP contains negative values' + '\n'
+
+    return is_valid, error_message
